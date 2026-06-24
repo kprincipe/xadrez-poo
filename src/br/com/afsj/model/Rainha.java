80b052d7cdf91ceddc41e507e5c98f1ee4075df1
baraunab <bbarauna.beatriz@gmail.com>
2026-06-23 21:30:24 -0400

    adicao de movimentos diagonais da rainha

--- src/br/com/afsj/model/Rainha.java
+++ src/br/com/afsj/model/Rainha.java
@@ -3,6 +3,7 @@ package br.com.afsj.model;
 public class Rainha extends Peca {
 	@Override
 	public boolean movimentoOK(int x, int y) {
+	
 		// se peça não estiver no tabuleiro, pode ser movida para qualquer posição
 		if (posX == -1 && posY == -1) {
 			return true;
@@ -10,6 +11,30 @@ public class Rainha extends Peca {
 		
 		if (y == posY || x == posX) {
 			return true;
+		}
+		
+		// movimentos na diagonal ↖️↙️↘️↗️
+		for (int i = 0; i < 8; i++) {
+			// diagonal esquerda superior ↖ 
+			if (x == posX-i &&  y == posY-i) {
+				return true;
+			}
+			
+			// diagonal direita inferior ↘
+			if (x == posX+i && y == posY+i) {
+				return true;
+			}
+			
+			// diagona direita superior ️↗️
+			if (x == posX+i && y == posY-i) {
+				return true;	
+			} 
+			
+			// diagonal esquerda inferior ↙️
+			if (x == posX-i && y == posY+i) {
+				return true;	
+			} 
+				
 		}
 		
 		return false;
@@ -20,6 +45,7 @@ public class Rainha extends Peca {
 		if (y == posY || x == posX) {
 			return true;
 		}
+		
 		
 		return false;
 	}
