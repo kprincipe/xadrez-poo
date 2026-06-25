ec8c48724c30d8a5f0d7aed352199d7145cc6fa3
kprincipe <kinanprincipe@outlook.com>
2026-06-25 19:35:14 -0400

    movimento da rainha

--- src/br/com/afsj/model/Rainha.java
+++ src/br/com/afsj/model/Rainha.java
@@ -3,7 +3,8 @@ package br.com.afsj.model;
 public class Rainha extends Peca {
 	@Override
 	public boolean movimentoOK(int x, int y) {
-	
+		int distancia = (int)Math.sqrt(Math.pow((posX - x), 2) + Math.pow((posX - x), 2));
+
 		// se peça não estiver no tabuleiro, pode ser movida para qualquer posição
 		if (posX == -1 && posY == -1) {
 			return true;
@@ -14,14 +15,26 @@ public class Rainha extends Peca {
 		}
 		
 		// movimentos na diagonal ↖️↙️↘️↗️
-		for (int i = 0; i < 8; i++) {
+		for (int i = 0; i <= 7; i++) {
 			// diagonal esquerda superior ↖ 
 			if (x == posX-i &&  y == posY-i) {
+				if (Tabuleiro.listaBrancas.pecaPosicao(posX - i, posY - i) != null) {
+					return false;
+				}
 				return true;
 			}
 			
 			// diagonal direita inferior ↘
 			if (x == posX+i && y == posY+i) {
+				for (int j = 0; j < distancia; ++j) {
+					if (Tabuleiro.listaBrancas.pecaPosicao(posX - i + j, posY + i - j) != null) {
+						return false;
+					}
+				}
+				
+				if (Tabuleiro.listaBrancas.pecaPosicao(posX + i - 1, posY + i - 1) != null) {
+					return false;
+				}
 				return true;
 			}
 			
@@ -32,11 +45,14 @@ public class Rainha extends Peca {
 			
 			// diagonal esquerda inferior ↙️
 			if (x == posX-i && y == posY+i) {
+				if (Tabuleiro.listaBrancas.pecaPosicao(posX - i + 1, posY + i - 1) != null) {
+					return false;
+				}
 				return true;	
 			} 
 				
 		}
-		
+
 		return false;
 	}
 	
